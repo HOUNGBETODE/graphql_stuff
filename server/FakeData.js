b5fd5ccad96baf5ede8fa26c7f6c81201894f476
HOUNGBETODE <moberenge@gmail.com>
2024-05-02 10:21:10 +0200

    Server-side

--- server/FakeData.js
+++ server/FakeData.js
@@ -1,67 +1,67 @@
-const UserList = null
+const UserList = [
+    {
+      id: 1,
+      name: "John",
+      username: "john",
+      age: 20,
+      nationality: "CANADA",
+      friends: [
+        {
+          id: 2,
+          name: "Pedro",
+          username: "PedroTech",
+          age: 20,
+          nationality: "BRAZIL",
+        },
+        {
+          id: 5,
+          name: "Kelly",
+          username: "kelly2019",
+          age: 5,
+          nationality: "CHILE",
+        },
+      ],
+    },
+    {
+      id: 2,
+      name: "Pedro",
+      username: "PedroTech",
+      age: 20,
+      nationality: "BRAZIL",
+    },
+    {
+      id: 3,
+      name: "Sarah",
+      username: "cameron",
+      age: 25,
+      nationality: "INDIA",
+      friends: [
+        {
+          id: 2,
+          name: "Pedro",
+          username: "PedroTech",
+          age: 20,
+          nationality: "BRAZIL",
+        },
+      ],
+    },
+    {
+      id: 4,
+      name: "Rafe",
+      username: "rafe123",
+      age: 60,
+      nationality: "GERMANY",
+    },
+    {
+      id: 5,
+      name: "Kelly",
+      username: "kelly2019",
+      age: 5,
+      nationality: "CHILE",
+    },
+];
 
-// const UserList = [
-//     {
-//       id: 1,
-//       name: "John",
-//       username: "john",
-//       age: 20,
-//       nationality: "CANADA",
-//       friends: [
-//         {
-//           id: 2,
-//           name: "Pedro",
-//           username: "PedroTech",
-//           age: 20,
-//           nationality: "BRAZIL",
-//         },
-//         {
-//           id: 5,
-//           name: "Kelly",
-//           username: "kelly2019",
-//           age: 5,
-//           nationality: "CHILE",
-//         },
-//       ],
-//     },
-//     {
-//       id: 2,
-//       name: "Pedro",
-//       username: "PedroTech",
-//       age: 20,
-//       nationality: "BRAZIL",
-//     },
-//     {
-//       id: 3,
-//       name: "Sarah",
-//       username: "cameron",
-//       age: 25,
-//       nationality: "INDIA",
-//       friends: [
-//         {
-//           id: 2,
-//           name: "Pedro",
-//           username: "PedroTech",
-//           age: 20,
-//           nationality: "BRAZIL",
-//         },
-//       ],
-//     },
-//     {
-//       id: 4,
-//       name: "Rafe",
-//       username: "rafe123",
-//       age: 60,
-//       nationality: "GERMANY",
-//     },
-//     {
-//       id: 5,
-//       name: "Kelly",
-//       username: "kelly2019",
-//       age: 5,
-//       nationality: "CHILE",
-//     },
-// ];
+// const UserList = null; (i use that to test my union entry well-working)
   
 const MovieList = [
     {
@@ -88,6 +88,6 @@ const MovieList = [
       yearOfPublication: 2035,
       isInTheaters: false,
     },
-]
+];
   
 module.exports = { UserList, MovieList }
